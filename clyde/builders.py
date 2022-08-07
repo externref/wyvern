90f9fba81016da06398bf5eeb2960a9dc51b0133
Sarthak <shiva02939@gmail.com>
2022-08-07 14:16:25 +0000

    Pending changes exported from your codespace

--- clyde/builders.py
+++ clyde/builders.py
@@ -1,6 +1,7 @@
 import datetime
 import typing
 
+
 @typing.final
 class DiscordObject:
     def __init__(
@@ -26,30 +27,42 @@ class DiscordObject:
     def id(self) -> int:
         return self._id
 
+
 @typing.final
 class Intents:
     NONE = 0
-    UNPRIVILEGED = 
 
     GUILDS = 1 << 0
+
     GUILD_MEMBERS = 1 << 1
+
     GUILD_BANS = 1 << 2
 
-    GUILD_EMOJIS_AND_STICKERS = 1 << 3
+    GUILD_EMOJIS = 1 << 3
+
     GUILD_INTEGRATIONS = 1 << 4
+
     GUILD_WEBHOOKS = 1 << 5
+
     GUILD_INVITES = 1 << 6
 
     GUILD_VOICE_STATES = 1 << 7
+
     GUILD_PRESENCES = 1 << 8
 
     GUILD_MESSAGES = 1 << 9
+
     GUILD_MESSAGE_REACTIONS = 1 << 10
+
     GUILD_MESSAGE_TYPING = 1 << 11
 
-    DIRECT_MESSAGES = 1 << 12
-    DIRECT_MESSAGE_REACTIONS = 1 << 13
-    DIRECT_MESSAGE_TYPING = 1 << 14
+    DM_MESSAGES = 1 << 12
+
+    DM_MESSAGE_REACTIONS = 1 << 13
+
+    DM_MESSAGE_TYPING = 1 << 14
+
+    MESSAGE_CONTENT = 1 << 15
 
     GUILD_SCHEDULED_EVENTS = 1 << 16
 
@@ -58,7 +71,7 @@ class Intents:
         *args: int,
     ) -> None:
         self._value = 0
-        #self.enabled: typing.MutableSet = set()
+        # self.enabled: typing.MutableSet = set()
         for value in args:
             self._value |= value
 
@@ -71,5 +84,3 @@ class Intents:
         intents = Intents()
         intents._value = value
         return intents
-
-
