5b8c5f7c84e10f37cae63fffff5a2809b781e89e
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2022-10-13 18:11:53 +0530

    fix `docs/conf.py`

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".")) 
-sys.path.insert(0, os.path.abspath("..")) 
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../"))
 
 project = "wyvern"
 copyright = "2022, sarthhh & FallenDeity"
