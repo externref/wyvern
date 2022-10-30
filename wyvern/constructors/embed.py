25a8eee3fe823d4c7317a2178fa68c086049cbbb
= <=>
2022-10-30 15:09:41 +0530

    added embeds support

--- wyvern/constructors/embed.py
+++ wyvern/constructors/embed.py
@@ -1,36 +1,82 @@
+# MIT License
+
+# Copyright (c) 2022 Sarthak
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from __future__ import annotations
 
-import dataclasses
 import datetime
 import typing
 
+import attrs
 
-@dataclasses.dataclass
+__all__: tuple[str, ...] = ("EmbedAuthor", "EmbedFooter", "EmbedField", "Embed", "EmbedConstructor")
+
+
+@typing.final
+@attrs.define(slots=True)
 class EmbedAuthor:
+    """Represents an embed author."""
+
     name: str
+    """Name of the author."""
     url: str | None = None
+    """URL that the author points to."""
     icon_url: str | None = None
+    """URL of the author icon"""
     proxy_icon_url: str | None = None
+    """Proxy URL of icon."""
 
     def to_payload(self) -> dict[str, str | None]:
+        """Converts the author to its payload."""
         return {"name": self.name, "url": self.url, "icon_url": self.icon_url}
 
 
-@dataclasses.dataclass
+@typing.final
+@attrs.define(slots=True)
 class EmbedFooter:
+    """Represents an embed footer"""
+
     text: str
+    """The text in the footer."""
     icon_url: str | None = None
+    """URL of the footer icon."""
     proxy_icon_url: str | None = None
+    """Proxy URL of the icon"""
 
     def to_payload(self) -> dict[str, str | None]:
+        """Converts the footer to its payload."""
         return {"text": self.text, "icon_url": self.icon_url}
 
 
-@dataclasses.dataclass
+@typing.final
+@attrs.define(slots=True)
 class EmbedField:
+    """Represnts an embed field."""
+
     name: str
+    """Name of the field."""
     value: str
+    """Value of the field."""
     inline: bool = True
+    """Inline value for the file, defaults to True."""
 
     def to_payload(self) -> dict[str, str | bool]:
         return {"name": self.name, "value": self.value, "inline": self.inline}
@@ -56,12 +102,7 @@ class EmbedConstructor:
 
     """
 
-    _payload: dict[str, typing.Any] = {"type": "rich", "fields": []}
-    _title: str | None = None
-    _description: str | None = None
-    _color: int = 0
-    _url: str | None = None
-    _timestamp: datetime.datetime | None = None
+    __slots__: tuple[str, ...] = ("_payload",)
 
     def __init__(
         self,
@@ -69,39 +110,17 @@ class EmbedConstructor:
         title: str | None = None,
         description: str | None = None,
         color: int = 0,
+        colour: int = 0,
         url: str | None = None,
         timestamp: datetime.datetime | None = None,
     ) -> None:
+
+        self._payload: dict[str, typing.Any] = {"type": "rich", "fields": []}
         self._payload["title"] = title
         self._payload["description"] = description
-        self._payload["color"] = color
+        self._payload["color"] = color or colour
         self._payload["url"] = url
         self._payload["timestamp"] = timestamp
-
-    @property
-    def title(self) -> str | None:
-        """Title of the embed."""
-        return self._payload.get("title")
-
-    @property
-    def description(self) -> str | None:
-        """Description of the embed."""
-        return self._payload.get("description")
-
-    @property
-    def color(self) -> int:
-        """Color of the embed."""
-        return self._payload.get("color", 0)
-
-    @property
-    def url(self) -> str | None:
-        """Embed's URL."""
-        return self._payload.get("url")
-
-    @property
-    def timestamp(self) -> datetime.datetime | None:
-        """Timestamp of the embed."""
-        return datetime.datetime.fromtimestamp(ts) if (ts := self._payload.get("timestamp")) else None
 
     def add_field(self, *, name: str, value: str, inline: bool = True) -> "EmbedConstructor":
         """Adds a field to the embed.
@@ -172,6 +191,66 @@ class EmbedConstructor:
         self._payload["footer"] = footer.to_payload()
         return self
 
+    def edit_init(
+        self,
+        *,
+        title: str | None = None,
+        description: str | None = None,
+        color: int = 0,
+        colour: int = 0,
+        url: str | None = None,
+        timestamp: datetime.datetime | None = None,
+    ) -> "EmbedConstructor":
+        self._payload["title"] = title or self._payload.get("title")
+        self._payload["description"] = description or self._payload.get("description")
+        self._payload["color"] = color or colour or self._payload.get("color")
+        self._payload["url"] = url or self._payload.get("url")
+        self._payload["timestamp"] = timestamp or self._payload.get("timestamp")
+        return self
 
+    def build(self) -> "Embed":
+        return Embed(
+            payload=self._payload,
+            title=self._payload.get("title"),
+            description=self._payload.get("description"),
+            url=self._payload.get("url"),
+            color=self._payload.get("color", 0),
+            fields=[EmbedField(**kargs.to_payload()) for kargs in self._payload.get("fields", [])],
+            author=self._payload.get("author"),
+            footer=self._payload.get("footer"),
+        )
+
+
+@typing.final
+@attrs.define(slots=True, kw_only=True, eq=True, repr=True)
 class Embed:
-    ...
+    """
+    Read-only Embed class returned while parsing message objects.
+    To create a sendable embed, use [wyvern.constructors.embed.EmbedConstructor][] instead.
+    """
+
+    payload: dict[str, typing.Any]
+    """The raw payload."""
+    title: str | None = None
+    """Title of the embed."""
+    description: str | None = None
+    """Description fo the embed"""
+    url: str | None = None
+    """URL of the embed"""
+    color: int = 0
+    """Color of the embed."""
+    timestamp: datetime.datetime | None = None
+    """Timestamp of the embed."""
+    colour = color
+    """Alias for color."""
+    fields: list[EmbedField] = []
+    """List of fields related to this embed."""
+    author: EmbedAuthor | None = None
+    """Author of the embed."""
+    footer: EmbedFooter | None = None
+    """Footer of the embed."""
+
+    def to_constructor(self) -> EmbedConstructor:
+        embed = EmbedConstructor()
+        embed._payload = self.payload
+        return embed
