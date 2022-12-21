224c3dc7d9fc69abe9499fecdfc25eca8b062d5f
sarthhh <shiva02939@gmail.com>
2022-12-21 19:42:20 +0530

    add basic cli.

--- wyvern/__main__.py
+++ wyvern/__main__.py
@@ -1,15 +1,28 @@
-import sys
 import argparse
+import sys
+
+import colorama
+
 from wyvern import __version__
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-V", "-v", "--version", help="Check the version of library.", action="store_true")
 
+print(colorama.Fore.LIGHTCYAN_EX, end="")
+
 
 def main() -> None:
     args = parser.parse_args()
     if args.version is True:
-        print("Library version: ", __version__, "\nPython version: ", sys.version)
-
+        print(
+            "Library version:",
+            colorama.Style.BRIGHT,
+            __version__,
+            colorama.Style.NORMAL,
+            "\nPython version:",
+            colorama.Style.BRIGHT,
+            sys.version,
+            colorama.Style.RESET_ALL,
+        ) 
 
 main()
