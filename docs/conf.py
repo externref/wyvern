5078ae319949a05af86ca6ca1979bf25164cc808
sarthhh <shiva02939@gmail.com>
2023-03-10 06:21:07 +0530

    fix module not found errors

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,13 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os, sys 
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../"))
 
 project = "wyvern"
 copyright = "2023, sarthhh"
 author = "sarthhh"
 release = "0.2.0"
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.napoleon", "myst_parser"]
 intersphinx_mapping = {
@@ -23,9 +18,7 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 autodoc_inherit_docstrings = True
 html_css_files = [
-    'style.css',
+    "style.css",
 ]
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_static_path = ["_static"]
